e7fe162c3cac685640b46dcd26b2f3a41e9c5582
Roman Martinez <roman.martinez@wizeline.com>
2020-09-09 12:20:20 -0500

    Added instructions for basic pre-compiling with babel

---
+++ src/pre-compiled.js
@@ -0,0 +1,23 @@
+'use strict';
+class ComponentClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {counter: 0};
+    this.increment = this.increment.bind(this);
+  }
+  increment() {
+    this.setState(state => ({counter: state.counter+1}))
+  }
+  render() {
+    return (
+      <p>
+        Dynamic content. props: {this.props.name}, state: {this.state.counter} &nbsp;
+        <button key="increment-button" onClick={this.increment}>Increment</button>
+      </p>
+    );
+  }
+}
+ReactDOM.render(
+  <ComponentClass name="Jeff" />,
+  document.querySelector('#my-app-5')
+);
