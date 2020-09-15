b99d950e5bb52142dc17c265c17d5b4c8eee0b5c
Roman Martinez <roman.martinez@wizeline.com>
2020-09-15 10:51:47 -0500

    Created main <App /> component, moved <HelloWorld /> into its own file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './App';
 
-const HelloWorld = () => <p>Hello World!</p>;
-render(<HelloWorld />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
