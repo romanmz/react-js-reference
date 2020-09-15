299464b00c2ffde4e1c49da4e9447a6b47ba225d
Roman Martinez <roman.martinez@wizeline.com>
2020-09-15 11:45:36 -0500

    Added info about environment settings, and React.StrictMode

--- src/App.js
+++ src/App.js
@@ -6,33 +6,35 @@ import DetailsSpecific from './DetailsSpecific';
 
 const App = () => {
     return (
-        <main className="app-main">
-            <aside className="app-sidebar">
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/details/1">Details 1</Link>
-                        </li>
-                        <li>
-                            <Link to="/details/2">Details 2</Link>
-                        </li>
-                        <li>
-                            <Link to="/details/3">Details 3</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </aside>
-            <section className="app-content">
-                <Router>
-                    <HelloWorld path="/" />
-                    <DetailsGeneric path="/details/:id" />
-                    <DetailsSpecific path="/details/1" />
-                </Router>
-            </section>
-        </main>
+        <React.StrictMode>
+            <main className="app-main">
+                <aside className="app-sidebar">
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/details/1">Details 1</Link>
+                            </li>
+                            <li>
+                                <Link to="/details/2">Details 2</Link>
+                            </li>
+                            <li>
+                                <Link to="/details/3">Details 3</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </aside>
+                <section className="app-content">
+                    <Router>
+                        <HelloWorld path="/" />
+                        <DetailsGeneric path="/details/:id" />
+                        <DetailsSpecific path="/details/1" />
+                    </Router>
+                </section>
+            </main>
+        </React.StrictMode>
     );
 };
 
